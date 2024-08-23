f51e2e24570717e2b4cd0c5ae35b69038e2bf6f6
startechnoboy <115495273+startechnoboy@users.noreply.github.com>
2024-08-23 13:47:10 +0530

    css problem solve

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Example: All node_modules packages will be bundled into a separate chunk
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+
+          // Optionally, you can create more specific chunks based on your project needs
+          // Example:
+          if (id.includes('src/components/YourLargeComponent')) {
+            return 'large-component';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase the limit to suppress the warning
+  },
 })
