71b1447f4e27bba89a0851a8c3f92e1f2b8f237e
startechnoboy <115495273+startechnoboy@users.noreply.github.com>
2024-08-31 11:31:02 +0530

    tail problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
